9defe9055390e1ffadc6e79c75ca7b9c21afe1fe
roopre <roopre@kia.com>
2023-04-19 13:17:08 +0900

    Account 서버 초기 설정

---
+++ Account-Common/src/test/java/com/hmg/accountcommon/AccountCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmg.accountcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
